cea619c945b300c3f3efbc124a6002bf4fe01b66
hoanglam3232 <hoanglam3232@gmail.com>
2024-05-31 02:24:13 +0700

    create env and setup for mongo
    create env and init mongoDB connection

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 
-// In case run on other environment please uncomment this line
-// require('dotenv').config();
-// const port = process.env.PORT
-const port = 3000;
+// In case run on other environment please modify .env
+require("dotenv").config();
+const port = process.env.PORT;
+const mongo = process.env.MONGO_URI;
 
 app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+mongoose
+  .connect(mongo)
+  .then(() => {
+    console.log("connect to mongoDb");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
